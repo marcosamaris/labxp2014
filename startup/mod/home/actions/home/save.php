55acc4dbebd69ad222d3170d77638c8069bf17c5
antonio rui sena dos reis castro junior <antonio.junior4@penadinho.cec.ime.usp.br>
2014-05-14 14:53:16 -0300

    removendo views/default/river/EllObject e pages/home/all e add (todos do plugin home)

--- startup/mod/home/actions/home/save.php
+++ startup/mod/home/actions/home/save.php
@@ -24,7 +24,7 @@ $home_guid = $home->save();
 // if the home post was saved, we want to display the new post
 // otherwise, we want to register an error and forward back to the form
 if ($home_guid) {
-	add_to_river('river/ElggObject/create', 'create', $home->owner_guid, $home->guid);
+	add_to_river('river/object/home/create', 'create', $home->owner_guid, $home->guid);
 	
    system_message(elgg_echo('home:saved'));
    forward("home/");
--- startup/mod/home/views/default/river/object/home/create.php
+++ startup/mod/home/views/default/river/object/home/create.php
@@ -4,7 +4,11 @@
  */
 
 $object = $vars['item']->getObjectEntity();
-$subject = $vars['item']->getSubjectEntity();
+$excerpt = elgg_get_excerpt($object->description);
 
+$summary = elgg_echo("river:create:object:home", array($subject_link, $object_link));
 
-echo "TESTE";
+echo elgg_view('river/elements/layout', array(
+	'item' => $vars['item'],
+	'message' => $excerpt,
+));
