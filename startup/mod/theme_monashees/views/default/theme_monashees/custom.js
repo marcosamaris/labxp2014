c367025fb7d6a6ac5d891cb9b47581ab13c1255f
elainenaomi <elaine.n.watanabe@gmail.com>
2014-07-10 22:59:02 -0300

    Restauração dos arquivos de views nativos do elgg; adição de exemplos com adição dinamica de campos no home

--- startup/mod/theme_monashees/views/default/theme_monashees/custom.js
+++ startup/mod/theme_monashees/views/default/theme_monashees/custom.js
@@ -3,4 +3,5 @@ $(function (){
 	$("form.elgg-form-comments-add").removeClass("hidden").attr("style","display:none");
 	$("div.elgg-module-popup.elgg-likes").removeClass("hidden").attr("style","display:none");
 	
+	
 });  
